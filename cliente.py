001ead03904d242e24da99eb1813976fe4651179
rhuanruiz <54746864+rhuanruiz@users.noreply.github.com>
2021-12-17 07:40:11 -0300

    Alteração endereço IP

--- cliente.py
+++ cliente.py
@@ -1,5 +1,5 @@
 import socket
-HOST = '192.0.2.10'     # Endereco IP do Servidor
+HOST = '127.0.0.1'     # Endereco IP do Servidor
 PORT = 5000            # Porta que o Servidor esta
 tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 dest = (HOST, PORT)
